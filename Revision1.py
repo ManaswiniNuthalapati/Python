b00f88e5bba482a5a714096fadf19532f4a61032
Manaswini <nuthalapatimanaswini@gmail.com>
2025-08-24 22:11:05 +0530

    file added

---
+++ Revision1.py
@@ -0,0 +1,46 @@
+#1
+# Input a  number as a string and print The first digit & The last digit
+'''
+a=int(input())
+b=input()
+first=int(b[0])
+second=int(b[-1])
+print(first)
+print(second)
+
+
+#2
+# Store an integer in a variable, then change its value to a float and print both.
+a=input()
+print(a)
+b=float(a)
+print(b)
+
+
+#3
+# Create two float variables and print their product.
+a=float(input())
+b=float(input())
+product=a*b
+print(product)
+print(int(product))
+
+#4
+# Input a number and print whether it is even
+a=int(input())
+print("Even:" ,a%2==0)
+
+#5
+# Take two integers and print the bitwise AND, OR, XOR results.
+a=int(input())
+b=int(input())
+print(a&b)
+print(a|b)
+print(a^b)
+'''
+#6
+# Assign two numbers and print their average without using division 
+a=int(input())
+b=int(input())
+c=(a+b)*0.5
+print(c)
