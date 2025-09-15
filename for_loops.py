2070b36ec8a9ab2dcfe04f320083523e00207725
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-15 22:12:44 +0530

    commited

--- for_loops.py
+++ for_loops.py
@@ -10,3 +10,15 @@ for i in range(x):
 # print first even numbers
 for i in range(20,0,-2):
   print(i)
+  
+# print multipplication of a number
+n=int(input())
+for i in range(n):
+  print(i*n)
+  
+# print sum of numbers from 1 to 50
+n=int(input())
+total=0
+for i in range(n+1):
+  total=total+i
+print(total)
