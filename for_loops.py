d78d8bf3ed898e6e11fc2c7acab13896003a5dbd
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-16 22:29:38 +0530

    commited

--- for_loops.py
+++ for_loops.py
@@ -38,3 +38,12 @@ for i in range(m):
 n = int(input())
 for i in range(1,11):
     print(n*i,end=" ")
+    
+# Given two number n1 and n2, n1 > n2. Find the differences between mathematical tables of n1 and n2 and print in a single line.    
+n1=int(input())
+n2=int(input())
+for i in range(1,11):
+    a=n1*i
+    b=n2*i
+    print(a-b,end=" ")
+
