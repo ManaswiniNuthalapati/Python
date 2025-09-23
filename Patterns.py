eb99e30308d99b6ac2185ade28ca494ee1767dc8
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-23 22:29:13 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -67,4 +67,28 @@ for i in range(1,n+1):
     for j in range(1,n):
         print("*",end=" ")
     print()
+    
+'''
+Reverse Right-angled Triangle
+****
+***
+**
+*
+'''
+n=5
+for i in range(1,n+1):
+    for j in range(1,n-i+2):
+        print("*",end=" ")
+    print()
 
+'''
+Reverse Right-angled Triangle with numbers
+55555
+4444
+333
+22
+1
+'''
+n=5
+for i in range(1,n+1):
+    for j in range(1,)
