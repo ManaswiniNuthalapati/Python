bfa04b6969a34aff6e0117bfac8cb9d665a65c04
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-23 21:46:20 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -54,5 +54,17 @@ for i in range(1,n+1):
     for j in range(1,i+1):
         print(i,end=" ")
     print()
-
+    
+'''
+Square Pattern
+****
+****
+****
+****
+'''
+n=5
+for i in range(1,n+1):
+    for j in range(1,n):
+        print("*",end=" ")
+    print()
 
