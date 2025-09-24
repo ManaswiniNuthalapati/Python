c7d23700fc878b3747bde7af45f9630b75256386
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-24 23:02:28 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -95,4 +95,31 @@ for i in range(1,n+1):
         print(n-i+1,end=" ")
     print()
     
+'''
+Reverse Number Triangle
+1234
+123
+12
+1
+'''
+n=5
+for i in range(1,n+1):
+    for j in range(1,n-i+2):
+        print(j,end=" ")
+    print()
+    
+'''
+Right-angled triangle with numbers starting from 1
+1
+2 3
+4 5 6
+7 8 9 10
+'''
+n=5
+num=1
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(num,end=" ")
+        num=num+1
+    print()    
     
