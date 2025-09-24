ae957431957e5a004d9fb8f52d980e6d41c8dde2
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-24 23:34:44 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -121,5 +121,18 @@ for i in range(1,n+1):
     for j in range(1,i+1):
         print(num,end=" ")
         num=num+1
-    print()    
+    print()  
     
+'''
+Right-aligned triangle of stars
+   *
+  **
+ ***
+ '''  
+n=5
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(i):
+        print("*",end=" ")
+    print()   
