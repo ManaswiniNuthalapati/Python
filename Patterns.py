9598f97d288d434daa962df06ee87257717d446e
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-24 23:44:31 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -135,4 +135,18 @@ for i in range(1,n+1):
         print(" ",end=" ")
     for k in range(i):
         print("*",end=" ")
-    print()   
+    print()  
+    
+'''
+right aligned triangle with numbers
+   1
+  22
+ 333
+''' 
+n=5
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(i):
+        print(i,end=" ")
+    print()
