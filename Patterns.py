a9a05e8f34ef58eee71c4ced895e13d2c767e7ac
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-25 21:54:17 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -194,3 +194,17 @@ for i in range(1,n+1):
         print("*",end="")
     print()
     
+'''
+Pyramid for numbers
+1 1 1 1 1 
+  2 2 2 2 
+   3 3 3 
+    4 4 
+     5 
+'''
+n=5
+for i in range(1,n+1):
+  for j in range(i):
+    print(" ",end="")
+  for k in range(n-i+1):
+    print(i,end="")
