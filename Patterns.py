e366e7a4f600a235efffa0c75d4052bfd18b9f83
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-25 23:14:38 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -208,3 +208,19 @@ for i in range(1,n+1):
     print(" ",end="")
   for k in range(n-i+1):
     print(i,end="")
+    
+'''
+Reverse triangle with continous numbers
+1 2 3 4 5 
+  1 2 3 4 
+   1 2 3 
+    1 2 
+     1 
+'''
+n=5
+for i in range(1,n+1):
+    for j in range(i):
+        print(" ",end="")
+    for k in range(n-i+1):
+        print(k+1,end="")
+    print()
