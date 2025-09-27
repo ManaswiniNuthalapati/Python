9ecd1e592a01398dc36d5011650dc6d8e27b4df7
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-27 22:06:29 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -252,7 +252,7 @@ for i in range(1,n+1):
     print()
     
 '''
-Pyramid of odd numbers
+Pyramid of odd numbers(low-->up)
     *
    ***
   *****
@@ -265,5 +265,20 @@ for i in range(1,n+1):
         print(" ",end="")
     for k in range(2*i-1):
         print("*",end=" ")
+    print()
+    
+'''
+Pyramid of odd numbers(up-->low)
+  * * * * * * * 
+    * * * * * 
+      * * * 
+        * 
+'''
+n=5
+for i in range(n-1,0,-1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(2*i-1):
+        print("*",end="")
     print()
     
