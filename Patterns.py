d77c15754fdc2bf5246b6d0e4d5418258bb95c02
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-27 22:09:31 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -277,8 +277,26 @@ Pyramid of odd numbers(up-->low)
 n=5
 for i in range(n-1,0,-1):
     for j in range(n-i):
-        print(" ",end="")
+        print(" ",end=" ")
     for k in range(2*i-1):
-        print("*",end="")
+        print("*",end=" ")
     print()
+    
+'''
+Diamond Full Pattern
+      * 
+    * * * 
+  * * * * * 
+* * * * * * * 
+  * * * * * 
+    * * * 
+      * 
+'''
+n=4
+for i in range(1,n+1):
+    for j in range(n-i):
+        print(" ",end=" ")
+    for k in range(2*i-1):
+        print("*",end=" ")
+    print()        
     
