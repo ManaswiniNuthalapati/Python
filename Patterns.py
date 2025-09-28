59bffe31eb47dd47b1e7f5285b7bb009327b787d
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-28 20:42:06 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -304,5 +304,27 @@ for i in range(n-1,0,-1):
         print(" ",end=" ")
     for k in range(2*i-1):
         print("*",end=" ")
-    print()       
+    print()      
+    
+'''
+HourGlass Pattern
+*****
+ ***
+  *
+ ***
+*****
+'''
+n=3
+for i in range(n,1,-1):
+  for j in range(n-i):
+    print(" ",end=" ")
+  for k in range(2*i-1):
+    print("*",end=" ")
+  print()
+for i in range(1,n+1):
+  for j in range(n-i):
+    print(" ",end=" ")
+  for k in range(2*i-1):
+    print("*",end=" ")
+  print() 
     
