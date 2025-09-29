754e1967c8be35edfa44dcd651493fd1574d50dc
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-29 19:46:55 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -399,7 +399,7 @@ Hollow Right-angled Triangle
 '''
 n=5
 for i in range(1,n+1):
-  for j in range(1,i+1):
+  for j in range(1,n+1):
     if i==n or j==1 or j==i:
       print("*",end=" ")
     else:
