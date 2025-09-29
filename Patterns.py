fa73d48d146705de25503ca6f2ff36078a226df6
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-29 16:30:20 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -351,3 +351,22 @@ for i in range(n+1,0,-1):
   for j in range(i):
     print(chr(65+j),end=" ")
   print()
+  
+'''
+Hollow Square
+
+*****
+*   *
+*   *
+*   *
+*****
+'''
+n=5
+for i in range(1,n+1):
+  for j in range(1,n+1):
+    if i==1 or i==n or j==1  or j==n:
+      print("*",end="")
+    else:
+      print(" ",end="")
+  print()
+ 
