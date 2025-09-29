38b33e2b1d444706ce38dd1b382657707a88bab0
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-29 19:45:50 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -389,3 +389,19 @@ for i in range(n):
       print(" ",end=" ")
   print()
 
+'''
+Hollow Right-angled Triangle
+* 
+* * 
+*   * 
+*     * 
+* * * * * 
+'''
+n=5
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    if i==n or j==1 or j==i:
+      print("*",end=" ")
+    else:
+      print(" ",end=" ")
+  print()
