35d5b0995c5599be0305a336c8b95f5d826f0abc
Manaswini <nuthalapatimanaswini@gmail.com>
2025-09-29 18:39:25 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -370,3 +370,22 @@ for i in range(1,n+1):
       print(" ",end="")
   print()
  
+ 
+'''
+X shape Pattern
+*       * 
+  *   *   
+    *     
+  *   *   
+*       * 
+
+'''
+n=5
+for i in range(n):
+  for j in range(n):
+    if i==j or i+j==n-1:
+      print("*",end=" ")
+    else:
+      print(" ",end=" ")
+  print()
+
