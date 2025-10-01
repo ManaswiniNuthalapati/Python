70c69726f8628141e0beae37c314d67ca53af655
Manaswini Nuthalapati <nuthalapatimanaswini@gmail.com>
2025-10-01 17:15:35 +0530

    Update for_loops.py

--- for_loops.py
+++ for_loops.py
@@ -114,6 +114,13 @@ for ch in text:
     if ch in vowels:
         continue  
     print(ch, end="")
+
+# Factorial of a number
+n = int(input())
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+print(fact)
     
 
 
