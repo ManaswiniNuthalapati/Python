7e7c5efb6dc963ab75a308b1576e549e9b4ac13b
Manaswini <nuthalapatimanaswini@gmail.com>
2025-10-06 20:08:32 +0530

    file updated

--- Patterns.py
+++ Patterns.py
@@ -454,3 +454,20 @@ for i in range(1,n+1):
     print(num,end=" ")
     num=num*(i-k)//k
   print()
+  
+'''
+Number Pyramid (Palindrome)
+   1
+  121
+ 12321
+1234321
+'''
+n=int(input())
+for i in range(1,n+1):
+  for j in range(1,n-i+1):
+    print(" ",end=" ")
+  for k in range(1,i+1):
+    print(k,end=" ")
+  for l in range(i-1,0,-1):
+    print(l,end=" ")
+  print()
