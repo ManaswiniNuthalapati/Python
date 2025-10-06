a795e2f35b146c991b81ad4474d43211cafbe852
Manaswini <nuthalapatimanaswini@gmail.com>
2025-10-06 19:28:40 +0530

    file updated

--- Patterns.py
+++ Patterns.py
@@ -405,3 +405,14 @@ for i in range(1,n+1):
     else:
       print(" ",end=" ")
   print()
+  
+  '''
+Rectangle of Stars
+*****
+*****
+*****'''
+n=int(input())
+for i in range(1,n-1):
+  for j in range(1,n+1):
+    print("*",end=" ")
+  print()
