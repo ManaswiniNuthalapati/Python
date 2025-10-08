f1c159b312c4641518f98b03c0608949a32d4a9f
Manaswini <nuthalapatimanaswini@gmail.com>
2025-10-08 23:10:45 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -528,5 +528,22 @@ for i in range(n-1,0,-1):
     else:
       print(" ",end=" ")
   print()
+  
+'''
+Inverted Triangle
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+'''
+n=int(input())
+for i in range(1,n+1):
+  for j in range(1,i+1):
+    if (i+j)%2==0:
+      print(1,end=" ")
+    else:
+      print(0,end=" ")
+  print()
 
   
