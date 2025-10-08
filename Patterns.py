f6191deed3dc6d246fd8987bedae2d3582d64998
Manaswini <nuthalapatimanaswini@gmail.com>
2025-10-08 22:26:23 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -496,6 +496,37 @@ for i in range(n,0,-1):
   print("*"*i,end="")
   print(" "*(2*(n-i)),end="")
   print("*"*i)
-  
+
+'''
+Hollow Diamond
+        * 
+      *   * 
+    *       * 
+  *           * 
+*               * 
+  *           * 
+    *       * 
+      *   * 
+        * 
+'''
+n=int(input())
+for i in range(1,n+1):
+  for j in range(1,n-i+1):
+    print(" ",end=" ")
+  for k in range(1,2*i):
+    if k==1 or k==2*i-1:
+      print("*",end=" ")
+    else:
+      print(" ",end=" ")
+  print()
+for i in range(n-1,0,-1):
+  for j in range(1,n-i+1):
+    print(" ",end=" ")
+  for k in range(1,2*i):
+    if k==1 or k==2*i-1:
+      print("*",end=" ")
+    else:
+      print(" ",end=" ")
+  print()
 
   
