7905569095450acc52e8084d51dc85ea00c621f7
Manaswini <nuthalapatimanaswini@gmail.com>
2025-10-09 22:59:07 +0530

    file added

--- Patterns.py
+++ Patterns.py
@@ -545,5 +545,35 @@ for i in range(1,n+1):
     else:
       print(0,end=" ")
   print()
+  
+'''
+    1
+   121
+  12321
+ 1234321
+123454321
+ 1234321
+  12321
+   121
+    1
+'''
+n=int(input())
+for i in range(1,n+1):
+  for j in range(1,n-i+1):
+    print(" ",end=" ")
+  for k in range(1,i+1):
+    print(k,end=" ")
+  for l in range(i-1,0,-1):
+    print(l,end=" ")
+  print()
+for i in range(n-1,0,-1):
+  for j in range(1,n-i+1):
+    print(" ",end=" ")
+  for k in range(1,i+1):
+    print(k,end=" ")
+  for l in range(i-1,0,-1):
+    print(l,end=" ")
+  print()
+
 
   
