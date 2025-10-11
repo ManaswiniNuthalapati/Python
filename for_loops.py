8b75c742102d522fb546d6b004e7c3933643efdb
Manaswini <nuthalapatimanaswini@gmail.com>
2025-10-11 22:31:21 +0530

    file Updated

--- for_loops.py
+++ for_loops.py
@@ -170,6 +170,18 @@ for i in range(1,n+1):
   if (n%i==0):
     print(i)
 
+# Find the GCD Numbers
+a=int(input())
+b=int(input())
+if a<b:
+  smaller=a
+else:
+  smaller=b
+gcd=1
+for i in range(1,smaller+1):
+  if a%i==0 and b%i==0:
+    gcd=i
+print(gcd)
     
 
   
