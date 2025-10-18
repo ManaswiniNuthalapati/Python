3b1672c30b53acf96434eeabe3058d5a3fc72dbb
Manaswini <nuthalapatimanaswini@gmail.com>
2025-10-18 22:11:53 +0530

    file  updated

--- Strings1.py
+++ Strings1.py
@@ -35,3 +35,17 @@ if s==rev:
     print(True)
 else:
     print(False)
+    
+'''
+Character Frequency Counter
+Input: "banana"
+Output: {'b':1, 'a':3, 'n':2} (you can print formatted output).
+'''
+s="banana"
+freq={}
+for ch in s:
+  if ch in freq:
+    freq[ch]+=1
+  else:
+    freq[ch]=1
+print(freq)
