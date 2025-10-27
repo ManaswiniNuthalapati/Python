cce70142c2cff238b9c78c7589db024048f53c3b
Manaswini <nuthalapatimanaswini@gmail.com>
2025-10-27 22:56:01 +0530

    file updated

--- Lists.py
+++ Lists.py
@@ -4,4 +4,23 @@ Lists in Python-->dynamic arrays that store items in [ ].
 separated by commas.
 It is a sequence datatype used to store a collections of data.
 
+creating a list --> []
+accessing element from list through indexing
+-ve indexing
+size of the list --> len()
+adding elements 
+--> append()
+--> insert() - adding ele at specific position
+
+del keyword --> delete values by index
+remove() method --> removes the first occurancce of a specific value
+pop() --> removes and returns an item at specific index
+
+max() --> returns the largest value
+min() --> returns the smallest value
+
+sort() --> sorts elements in asc,des order
+reverse() --> reverses the elements
+
+
 """
