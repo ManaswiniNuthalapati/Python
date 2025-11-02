87d9f23ce5c54d7ac0e4294adfeb0f225a192260
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-02 23:28:20 +0530

    file added

--- Lists1.py
+++ Lists1.py
@@ -5,3 +5,10 @@ for i in num:
     if i%2==0:
         num.append(i)
 print(even)
+
+# Find all odd numbers and print them in a single line
+num=[10, 21, 4, 45, 66, 93, 11]
+print("Odd numbers:", end=" ")
+for i in num:
+    if i%2!=0:
+        print(i,end=" ")
