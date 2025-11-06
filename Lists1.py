72fdcc2d9fb0468ca2cabf1afb8a767f8775aca8
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-06 12:00:00 +0530

    code updated

--- Lists1.py
+++ Lists1.py
@@ -62,3 +62,14 @@ for n in nums:
 print(positive)
 print(negative)
 print(zeros)
+
+# Find All Pairs That Sum to a Target
+nums=[1, 2, 3, 4, 5, 6]
+target=7
+pairs=[]
+for i in range(len(nums)):
+    for j in range(i + 1, len(nums)):
+        if nums[i] + nums[j] == target:
+            pairs.append((nums[i], nums[j]))
+print(pairs)
+
