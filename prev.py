c930cce6039a685c166c8c2542298bff86112d31
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-09 22:51:54 +0530

    file updated

--- prev.py
+++ prev.py
@@ -44,3 +44,11 @@ for x in lst:
     total += x
     cum.append(total)
 print(cum)
+
+# Count Divisors
+num = int(input("Enter a number: "))
+count = 0
+for i in range(1, num + 1):
+    if num % i == 0:
+        count += 1
+print("Number of divisors:", count)
