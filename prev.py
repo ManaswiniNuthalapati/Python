b575e0015ec1181c374a6e72261229607b4082a3
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-12 23:06:30 +0530

    file updated

--- prev.py
+++ prev.py
@@ -106,3 +106,13 @@ for i in range(len(merged)):
         if merged[i] > merged[j]:
             merged[i], merged[j] = merged[j], merged[i]
 print("Sorted merged list:", merged)
+
+# Merge and Sort Lists
+a = [3, 1, 4]
+b = [2, 5, 0]
+merged = a + b
+for i in range(len(merged)):
+    for j in range(i+1, len(merged)):
+        if merged[i] > merged[j]:
+            merged[i], merged[j] = merged[j], merged[i]
+print("Sorted merged list:", merged)
