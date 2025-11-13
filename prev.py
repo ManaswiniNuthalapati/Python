46dad92a768c6b331e26341a187a345eae884056
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-13 22:57:01 +0530

    file updtted

--- prev.py
+++ prev.py
@@ -139,3 +139,11 @@ if rev == num:
     print("Palindrome ✅")
 else:
     print("Not Palindrome ❌")
+
+# Count Divisors
+num = int(input("Enter a number: "))
+count = 0
+for i in range(1, num + 1):
+    if num % i == 0:
+        count += 1
+print("Number of divisors:", count)
