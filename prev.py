83b0ef28d11aff453f9003cad9313686f38922e1
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-15 12:00:00 +0530

    file updated

--- prev.py
+++ prev.py
@@ -147,3 +147,11 @@ for i in range(1, num + 1):
     if num % i == 0:
         count += 1
 print("Number of divisors:", count)
+
+# Count Divisors
+num = int(input("Enter a number: "))
+count = 0
+for i in range(1, num + 1):
+    if num % i == 0:
+        count += 1
+print("Number of divisors:", count)
