9dae7ad064de9ad506ddbd515eb2007216114802
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-17 12:00:00 +0530

    file updated

--- prev.py
+++ prev.py
@@ -269,4 +269,12 @@ a, b = 0, 1
 for _ in range(10):
     print(a, end=" ")
     a, b = b, a + b
+    
+Remove duplicates from a list
+lst = [1,2,3,2,1,4,5,4]
+unique = []
+for i in lst:
+    if i not in unique:
+        unique.append(i)
+print(unique)
 
