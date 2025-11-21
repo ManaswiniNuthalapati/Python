8b99e06f1747d13ea3ed43c8a856b9cad7c81795
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-21 12:00:00 +0530

    file updated

--- prev.py
+++ prev.py
@@ -232,3 +232,12 @@ for i in s:
     if i != " ":
         result += i
 print(result)
+
+# reverse a number
+num = int(input("Enter a number: "))
+rev = 0
+while num > 0:
+    digit = num % 10        
+    rev = rev * 10 + digit  
+    num = num // 10         
+print("Reversed number:", rev)
