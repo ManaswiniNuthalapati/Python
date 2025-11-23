31d141ddfb9b6b2c41690657f1eaa9fa627b58d1
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-23 10:00:00 +0530

    Your commit message

--- prev.py
+++ prev.py
@@ -354,3 +354,8 @@ print(common)
 a = int(input())
 b = int(input())
 print(a + b)
+
+Problem 2: Find area of a circle
+r = float(input("Enter radius: "))
+area = 3.14159 * r * r
+print("Area =", area)
