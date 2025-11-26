d36984ea32dde4f97d4928cc1e3c04c823bb52fb
Manaswini <nuthalapatimanaswini@gmail.com>
2025-11-26 10:00:00 +0530

    Your commit message

--- prev.py
+++ prev.py
@@ -386,8 +386,6 @@ for i in range(1, n + 1):
 Problem 6: Factorial of a number
 n = int(input("Enter a number: "))
 fact = 1
-
 for i in range(1, n + 1):
     fact = fact * i
-
 print("Factorial =", fact)
