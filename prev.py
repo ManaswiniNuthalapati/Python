e9c7d89ac39f3dc7ba93904f1d089cfcb9bd2c5c
Manaswini <nuthalapatimanaswini@gmail.com>
2025-12-01 23:02:32 +0530

    file updated

--- prev.py
+++ prev.py
@@ -453,6 +453,13 @@ else:
 n = int(input())
 for i in range(1, 11):
     print(n, "x", i, "=", n * i)
+    
+Find Duplicate Elements in a List
+lst = [1, 2, 3, 2, 4, 3, 5]
+for i in range(len(lst)):
+    for j in range(i + 1, len(lst)):
+        if lst[i] == lst[j]:
+            print("Duplicate found:", lst[i])
 
 
 
