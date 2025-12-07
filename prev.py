ddb0236d80c3f81ab9b2354971ba0d7464b1c040
Manaswini <nuthalapatimanaswini@gmail.com>
2025-12-07 14:01:28 +0530

    file updated

--- prev.py
+++ prev.py
@@ -628,3 +628,12 @@ for i in a:
 for i in range(count):
     b.append(0)
 print(b)   
+
+# Sort a List
+a=[2,3,45,1,6,9,5]
+n=len(a)
+for i in range(n):
+    for j in range(i+1,n):
+        if a[i]>a[j]:
+            a[i],a[j]=a[j],a[i]
+print(a)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,8 @@
-a=111
-print(int(a,2))
+# Sort a List
+a=[2,3,45,1,6,9,5]
+n=len(a)
+for i in range(n):
+    for j in range(i+1,n):
+        if a[i]>a[j]:
+            a[i],a[j]=a[j],a[i]
+print(a)
