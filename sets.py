1a26c562c8794817dbc8848bb6c6e2b4f368bfeb
Manaswini <nuthalapatimanaswini@gmail.com>
2025-12-24 12:00:00 +0530

    Your commit message

---
+++ sets.py
@@ -0,0 +1,18 @@
+# Sets
+'''
+A set is an unordered collection of unique elements
+Written using { }
+Duplicates are automatically removed
+Unindexed (no positions like 0,1,2…)
+Mutable (you can add/remove items)
+'''
+# Important Set Methods
+'''
+add()       # Add element
+remove()    # Remove element (error if not exist)
+discard()   # Remove element (no error)
+union()     # Combine sets
+intersection() # Common elements
+difference()   # Elements in A not in B
+'''
+
