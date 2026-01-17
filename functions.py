18d61a5d11d36d2340471ab09f61e15a44ccefc5
Manaswini <nuthalapatimanaswini@gmail.com>
2026-01-17 22:14:59 +0530

    file updated

--- functions.py
+++ functions.py
@@ -103,9 +103,9 @@ print_numbers(5)
 
 # Check Positive or Negative
 def check_number(n):
-    if n > 0:
+    if n>0:
         print("Positive")
-    elif n < 0:
+    elif n<0:
         print("Negative")
     else:
         print("Zero")
