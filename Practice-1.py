379d068af7afa3cdbfd4a25f464015e9d59c01bc
Manaswini <nuthalapatimanaswini@gmail.com>
2026-01-29 23:42:32 +0530

    file updated

--- Practice-1.py
+++ Practice-1.py
@@ -51,6 +51,21 @@ s=s.upper()
 print(s)
 
 '''
+Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+Suppose the following input is supplied to the program: Hello world! Then, the output should be: UPPER CASE 1 LOWER CASE 9
+'''
+s="Hello World"
+upper=0
+lower=0
+for i in s:
+  if i.isupper():
+    upper+=1
+  if i.islower():
+    lower+=1
+print(upper)
+print(lower)
+
+'''
 Square Pattern
 ****
 ****
