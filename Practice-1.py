47738f3ba443b1adce4a900f7e0af6f544419370
Manaswini <nuthalapatimanaswini@gmail.com>
2026-01-30 23:25:10 +0530

    file updated

--- Practice-1.py
+++ Practice-1.py
@@ -188,3 +188,19 @@ for i in range(n,-1,-1):
   for k in range(2*i-1):
     print("*",end="")
   print()
+
+'''
+Hollow Square
+* * * *
+*     *
+*     *
+* * * *
+'''
+n=5
+for i in range(n):
+  for j in range(n):
+    if i==0 or i==n-1 or j==0 or j==n-1:
+      print("*",end=" ")
+    else:
+      print(" ",end=" ")
+  print()
