084f5ee45b1929b40e88ca0e896399056af5d308
Manaswini <nuthalapatimanaswini@gmail.com>
2026-01-30 22:40:23 +0530

    file updated

--- Practice-1.py
+++ Practice-1.py
@@ -158,3 +158,18 @@ for i in range(1,n+1):
   for j in range(1,i+1):
     print(i,end=" ")
   print()
+  
+'''
+Star Pyramid
+   *
+  ***
+ *****
+*******
+'''
+n=5
+for i in range(1,n+1):
+  for j in range(n-i):
+    print(" ",end="")
+  for k in range(2*i-1):
+    print("*",end="")
+  print()
