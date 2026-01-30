9483e16dbc8424a0f6e1231cc5ecbfa976eecee7
Manaswini <nuthalapatimanaswini@gmail.com>
2026-01-30 22:42:10 +0530

    file updated

--- Practice-1.py
+++ Practice-1.py
@@ -173,3 +173,18 @@ for i in range(1,n+1):
   for k in range(2*i-1):
     print("*",end="")
   print()
+  
+'''
+Inverted Pyramid
+*******
+ *****
+  ***
+   *
+'''
+n=5
+for i in range(n,-1,-1):
+  for j in range(n-i):
+    print(" ",end="")
+  for k in range(2*i-1):
+    print("*",end="")
+  print()
