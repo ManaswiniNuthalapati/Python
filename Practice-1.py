302b706d2eba173607497570b6ed89c7e9a69c3d
Manaswini <nuthalapatimanaswini@gmail.com>
2026-02-03 09:37:09 +0530

    file updated

--- Practice-1.py
+++ Practice-1.py
@@ -320,3 +320,16 @@ words=n.split(',')
 words.sort()
 res=",".join(words)
 print(res)
+
+'''
+Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then 
+check whether they are divisible by 5 or not.
+The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+'''
+n=input().split(",")
+res=[]
+for i in n:
+  dec=int(a,2)
+  if dec%5==0:
+    res.append(i)
+print(res)
