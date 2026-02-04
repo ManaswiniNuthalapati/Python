1228dc6432feb6b7c99d1164b457ddd1d6cc7b93
Manaswini <nuthalapatimanaswini@gmail.com>
2026-02-04 18:00:00 +0530

    Daily python practice

--- Practice-1.py
+++ Practice-1.py
@@ -386,6 +386,9 @@ Write a program which can generate a tuple where the values are square of number
 between 1 and 20 (both included).
 Then print the first half values in the tuple.
 '''
+t = tuple(i*i for i in range(1, 21))
+mid = len(t) // 2
+print(t[:mid])
 
 '''
 Write a program which can generate a tuple where the values are square of numbers 
