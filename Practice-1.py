b8d1b466944ef7b83b82f173d078da54d49f617e
Manaswini <nuthalapatimanaswini@gmail.com>
2026-02-07 18:00:00 +0530

    Daily python practice

--- Practice-1.py
+++ Practice-1.py
@@ -513,7 +513,7 @@ expected_sum=n*(n+1)//2
 actual_sum=sum(arr)
 print(expected_sum - actual_sum)
 
-Longest Word in a Sentence
+# Longest Word in a Sentence
 s = input("Enter sentence: ")
 words = s.split()
 longest = words[0]
