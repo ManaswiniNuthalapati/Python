e1f771fb8f4c25706c67e0ecdf35fbeb97fd4f38
Manaswini <nuthalapatimanaswini@gmail.com>
2026-02-08 18:00:00 +0530

    Daily python practice

--- Practice-1.py
+++ Practice-1.py
@@ -495,13 +495,13 @@ else:
     print("Array is not sorted")
     
 # Move All Zeros to End (Maintain Order)
-arr = list(map(int, input().split()))
-result = []
-zero_count = 0
+arr=list(map(int, input().split()))
+result=[]
+zero_count=0
 for x in arr:
-    if x != 0:
+    if x!=0:
         result.append(x)
     else:
-        zero_count += 1
-result.extend([0] * zero_count)
+        zero_count+=1
+result.extend([0]*zero_count)
 print(result)
