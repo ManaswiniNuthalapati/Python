40b646e8625d86cc3be3e459f6c6947c4aa083cb
Manaswini <nuthalapatimanaswini@gmail.com>
2026-02-11 18:00:00 +0530

    Daily python practice

--- Practice-1.py
+++ Practice-1.py
@@ -524,7 +524,7 @@ print(longest)
 
 # Rotate Array by K Steps
 def rotate(nums, k):
-    n = len(nums)
-    k = k % n
-    return nums[-k:] + nums[:-k]
-print(rotate([1,2,3,4,5,6,7], 3))
+    n=len(nums)
+    k=k%n
+    return nums[-k:]+nums[:-k]
+print(rotate([1,2,3,4,5,6,7],3))
