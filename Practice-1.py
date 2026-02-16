39f841ecfc66067f07765ab9a496a74b00e454d6
Manaswini <nuthalapatimanaswini@gmail.com>
2026-02-16 18:00:00 +0530

    Daily python practice

--- Practice-1.py
+++ Practice-1.py
@@ -562,3 +562,9 @@ def is_rotation(s1,s2):
     return s2 in (s1+s1)
 print(is_rotation("abcde","cdeab")) 
 
+# Intersection of Two Arrays
+def intersection(nums1, nums2):
+    return list(set(nums1)&set(nums2))
+print(intersection([1,2,2,1], [2,2])) 
+
+
