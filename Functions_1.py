fac3f94c6d25d3c991ce318fc64e8f701cd21ee3
Manaswini <nuthalapatimanaswini@gmail.com>
2026-03-02 18:00:00 +0530

    Daily python practice

--- Functions_1.py
+++ Functions_1.py
@@ -346,3 +346,13 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
+
+# Count Frequency of Elements
+def frequency(arr):
+    freq = {}
+    for num in arr:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+    return freq
