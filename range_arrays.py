432b37907e334eb4ce729cfe352ed84e4ed672ee
Manaswini <nuthalapatimanaswini@gmail.com>
2026-03-04 18:00:00 +0530

    Daily python practice

---
+++ range_arrays.py
@@ -0,0 +1,15 @@
+'''
+Python range() Function
+1. Definition
+The range() function returns a sequence of numbers, starting from 0 by default, 
+incrementing by 1, and stopping before the specified number.
+It is commonly used with for loops to iterate a specific number of times.
+
+Syntax
+range(start, stop, step)
+Parameters
+Parameter	         Description
+start	             Starting number of the sequence. Default = 0
+stop	             The sequence stops before this value
+step	             Increment value between numbers. Default = 1
+'''
