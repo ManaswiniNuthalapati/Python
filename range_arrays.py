36555606c8728a54666283f6ecd2abe20fb981ce
Manaswini <nuthalapatimanaswini@gmail.com>
2026-03-04 18:00:00 +0530

    Daily python practice

--- range_arrays.py
+++ range_arrays.py
@@ -13,3 +13,37 @@ start	             Starting number of the sequence. Default = 0
 stop	             The sequence stops before this value
 step	             Increment value between numbers. Default = 1
 '''
+
+'''
+Python Arrays (According to W3Schools)
+1. What is an Array?
+An array is a special variable that can store multiple values in a single variable.
+Instead of creating many variables like:
+car1 = "Ford"
+car2 = "Volvo"
+car3 = "BMW"
+
+You can store them together in one array:
+cars = ["Ford", "Volvo", "BMW"]
+This makes it easier to manage large collections of data.
+
+Python does not have built-in arrays like some other languages.
+Instead, Python uses lists as arrays.
+
+Array Methods (Important)
+
+Python arrays (lists) have many built-in methods.
+
+Method	    Description
+append()	Adds element at end
+insert()	Adds element at specific position
+remove()	Removes element by value
+pop()	    Removes element by index
+sort()	    Sorts array
+reverse()	Reverses array
+count()	    Counts occurrences
+index()	    Returns index of element
+clear()	    Removes all elements
+copy()	    Returns copy of array
+'''
+
