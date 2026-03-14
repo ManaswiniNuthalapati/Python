e332d8906cfdcd88a3b04a98a75c6a120970fd03
Manaswini <nuthalapatimanaswini@gmail.com>
2026-03-14 18:00:00 +0530

    Daily python practice

--- Arrays.py
+++ Arrays.py
@@ -191,3 +191,11 @@ for i in range(len(n)):
     if n[i] not in dup:
         dup.append(n[i])
 print(dup)
+
+# Two Sum
+nums = [2,7,11,15]
+target = 9
+for i in range(len(nums)):
+    for j in range(i+1, len(nums)):
+        if nums[i] + nums[j] == target:
+            print([i, j])
