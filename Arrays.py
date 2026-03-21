20f7973a4c3ee5931f3bdc6902f95ab454f897e4
Manaswini <nuthalapatimanaswini@gmail.com>
2026-03-21 18:00:00 +0530

    Daily python practice

--- Arrays.py
+++ Arrays.py
@@ -268,3 +268,10 @@ def smallest(nums):
     return min_val
 
 # Reverse Array
+def reverse(nums):
+    left, right = 0, len(nums)-1
+    while left < right:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1
+    return nums
